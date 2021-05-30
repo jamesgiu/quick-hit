e347395de38e569914bdb0d192856197b8d0bea1
james <s3541398@student.rmit.edu.au>
2021-05-30 15:31:21 +1000

    Improved not found page + styling tweaks for player cards

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './NotFound.css';
+import {Header, Icon} from "semantic-ui-react";
 
 function NotFound() {
     return (
         <div className="not-found">
-            Not found page
+            <Header as={"h2"} icon>
+                <Icon name='warning' circular/>
+                <Header.Content>Page not found</Header.Content>
+            </Header>
         </div>
     );
 }
