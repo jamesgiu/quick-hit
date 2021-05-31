25d6e5b090e351140dfacce361c741e3e2511717
james <s3541398@student.rmit.edu.au>
2021-05-31 21:07:08 +1000

    Speed up animations, fix ladder to sort by ELO and add toggle for zero game players to be hidden

--- src/components/Ladder/Ladder.tsx
+++ src/components/Ladder/Ladder.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './Ladder.css';
 import {DB_Player} from "../../types/database/models";
-import {Button, Header, Icon, Transition} from "semantic-ui-react";
+import {Button, Checkbox, Header, Icon, Transition} from "semantic-ui-react";
 import PlayerCard from "./PlayerCard/PlayerCard";
 import NewPlayer from './NewPlayer/NewPlayer';
 import NewGame from "./NewGame/NewGame";
@@ -15,19 +15,30 @@ type LadderStyle = 'vertical' | 'horizontal';
 function Ladder() {
     const [loaderData, setLoaderData] = useState<LoaderData>({playersMap: new Map<string, DB_Player>(), matches: [], loading: true});
     const [ladderStyle, toggleLadderStyle] = useState<LadderStyle>('horizontal');
+    const [hideZeroGamePlayers, setHideZeroGamePlayers] = useState<boolean>(true);
 
     const renderPlayers = (): JSX.Element[] => {
         const playerItems: JSX.Element[] = [];
         Array.from(loaderData.playersMap.values()).forEach((player) => {
+            const winLoss = getWinLossForPlayer(player.id, loaderData.matches);
+
             const playerCard = (
-                <PlayerCard player={player} winLoss={getWinLossForPlayer(player.id, loaderData.matches)}/>
+                <PlayerCard player={player} winLoss={winLoss}/>
             );
 
-            playerItems.push(playerCard);
+            // If we are hiding zero game players, then only push if they have played a game
+            if (hideZeroGamePlayers) {
+                if (winLoss.wins + winLoss.losses > 0) {
+                    playerItems.push(playerCard);
+                }
+            }
+            else {
+                playerItems.push(playerCard);
+            }
         });
 
         // Sorting the player items by wins.
-        playerItems.sort((player1, player2) => {return player2.props.winLoss.wins - player1.props.winLoss.wins});
+        playerItems.sort((player1, player2) => {return player2.props.player.elo - player1.props.player.elo});
         return playerItems;
     }
 
@@ -41,6 +52,12 @@ function Ladder() {
                 <Icon name='trophy' circular/>
                 <Header.Content>Ladder</Header.Content>
             </Header>
+            <div className={"toggle-zero-game-players"}>
+                <div>
+                    Hide players who haven't played a game:
+                </div>
+                <Checkbox toggle defaultChecked onChange={()=>setHideZeroGamePlayers(!hideZeroGamePlayers)} />
+            </div>
             <QHDataLoader dataReceivedCallback={receiveDataFromLoader}/>
             <Transition visible={!loaderData.loading}>
                 <span>
