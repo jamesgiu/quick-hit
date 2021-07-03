e1921ec2abf342de72e038c9083986d9c2e0b45c
ChrisNeedham24 <christopherneedham24@gmail.com>
2021-07-03 22:55:41 +1000

    QH-7 Added tournament name, player ranks, strikethrough to losers, restructured new tournament modal.

--- src/components/Tournament/Tournament.tsx
+++ src/components/Tournament/Tournament.tsx
@@ -1,23 +1,23 @@
 import { useState } from "react";
-import { Button, Form, Icon, Input, List, Message, Modal } from "semantic-ui-react";
+import { Button, Form, Header, Icon, Message, Modal, Table } from "semantic-ui-react";
 import { QuickHitAPI } from "../../api/QuickHitAPI";
 import { TTDataPropsTypeCombined } from "../../containers/shared";
-import { DbMatch, DbPlayer, DbTournament, DbTournamentMatch, getTodaysDate } from "../../types/database/models";
+import { DbPlayer, DbTournament, DbTournamentMatch, getTodaysDate } from "../../types/database/models";
 import { makeErrorToast, makeSuccessToast } from "../Toast/Toast";
 import { v4 as uuidv4 } from "uuid";
 import "./Tournament.css";
 import { getPlayersMap } from "../QHDataLoader/QHDataLoader";
 
-// Add strikethrough to game losers.
 // Looks like the match is updated on the UI even if the request fails.
 // Make sure new tournament form validation works.
-// Add player rank in brackets next to game name.
 // Add past tournaments.
-// Add tournament title to top of page.
+// Look at query param sorting for tournaments rather than doing it client side.
+// Sometimes difficult to click confirm match button.
 
 // DO LAST:
 // Pull in main.
 // Run linter.
+// Run prettier.
 
 function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
     const [newTournamentModalOpen, openNewTournamentModal] = useState<boolean>(false);
@@ -28,11 +28,41 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
     const [homePlayerEnteringScore, setHomePlayerEnteringScore] = useState<number | undefined>(undefined);
     const [awayPlayerEnteringScore, setAwayPlayerEnteringScore] = useState<number | undefined>(undefined);
     const [matchEntering, setMatchEntering] = useState<DbTournamentMatch | undefined>(undefined);
+    const [confirmingMatchScore, setConfirmingMatchScore] = useState<boolean>(false);
+    const [startingNewTournament, setStartingNewTournament] = useState<boolean>(false);
+
     const sortedPlayers = props.players.sort((p1, p2) => p2.elo - p1.elo)
     const sortedTournaments = props.tournaments.sort((t1, t2) => t2.start_date.localeCompare(t1.start_date));
     const playersMap = getPlayersMap(props.players);
 
+    const playerRanksMap = new Map<string, number>();
+
+    if (sortedTournaments[0]?.matches) {
+        playerRanksMap.set(sortedTournaments[0].matches[0].home_player_id, 1);
+        playerRanksMap.set(sortedTournaments[0].matches[0].away_player_id, 8);
+        playerRanksMap.set(sortedTournaments[0].matches[1].home_player_id, 4);
+        playerRanksMap.set(sortedTournaments[0].matches[1].away_player_id, 5);
+        playerRanksMap.set(sortedTournaments[0].matches[2].home_player_id, 2);
+        playerRanksMap.set(sortedTournaments[0].matches[2].away_player_id, 7);
+        playerRanksMap.set(sortedTournaments[0].matches[3].home_player_id, 3);
+        playerRanksMap.set(sortedTournaments[0].matches[3].away_player_id, 6);
+    }
+
     const startNewTournament = (players: DbPlayer[], name: string) => {
+        const onSuccess = () => {
+            makeSuccessToast("Tournament started!", `A new tournament ${name} has been started!`);
+            setStartingNewTournament(false);
+            openNewTournamentModal(false);
+            props.setForceRefresh(true);
+        };
+
+        const onError = (errorMsg: string) => {
+            makeErrorToast("Could not start tournament", errorMsg);
+            setStartingNewTournament(false);
+        };
+
+        setStartingNewTournament(true);
+
         const tournamentMatches: DbTournamentMatch[] = [];
         tournamentMatches.push({
             "match_number": 0,
@@ -61,19 +91,25 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
             "matches": tournamentMatches
         };
     
-        QuickHitAPI.addUpdateTournament(
-            newTournament, 
-            () => {
-                makeSuccessToast("Tournament started!", `A new tournament ${name} has been started!`);
-                props.setForceRefresh(true);
-            },
-            (errorString) => makeErrorToast("Could not start tournament", errorString));
+        QuickHitAPI.addUpdateTournament(newTournament, onSuccess, onError);
     };
+
+    const homeWon = (match: DbTournamentMatch): boolean | undefined => {
+        if (match && match.home_score !== undefined && match.away_score !== undefined) {
+            return match.home_score > match.away_score;
+        } else {
+            return undefined;
+        }
+    }
 
     const getTeamItem = (match: DbTournamentMatch, playersMap: Map<string, DbPlayer>): JSX.Element => {
         return (
         <div>
-            {match ? playersMap.get(match.home_player_id)?.name : ""} 
+            <span className={homeWon(match) === false ?  "match-loser" : undefined}>
+                {match?.home_player_id
+                 ? `(${playerRanksMap.get(match.home_player_id)}) ${playersMap.get(match.home_player_id)?.name}`
+                 : "TBD"}
+            </span>
             <span className={match &&
                              match.home_player_id &&
                              match.away_player_id &&
@@ -85,7 +121,11 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
                 : <span>VS</span>
                 }
             </span>
-            {match ? playersMap.get(match.away_player_id)?.name : ""}
+            <span className={homeWon(match) === true ? "match-loser" : undefined}>
+                {match?.away_player_id
+                 ? `(${playerRanksMap.get(match.away_player_id)}) ${playersMap.get(match.away_player_id)?.name}`
+                 : "TBD"}
+            </span>
         </div>
         );
     };
@@ -103,13 +143,17 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
             setAwayPlayerEnteringScore(undefined);
             makeSuccessToast("Game entered!", "The tournament marches on!");
             openEnterGameModal(false);
+            setConfirmingMatchScore(false);
             props.setForceRefresh(true);
         };
 
         const onError = (errorMsg: string) => {
             makeErrorToast("Game not entered!", errorMsg);
+            setConfirmingMatchScore(false);
             props.setForceRefresh(true);
         };
+
+        setConfirmingMatchScore(true);
 
         if (homePlayerEnteringScore === awayPlayerEnteringScore) {
             makeErrorToast("Come on man", "Winning player score must be higher than losing player score");
@@ -188,17 +232,30 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
             return <span>Who will it be?</span>;
         }
     };
-    
 
     return (
         <div>
+        {sortedTournaments.length > 0 &&
+         sortedTournaments[0].matches[6] &&
+         sortedTournaments[0].matches[6].home_score !== undefined
+            ? <div>
+                <div className={"congrats-div"}>
+                    Congratulations {getWinner()}!
+                </div>
+                <Button onClick={() => openNewTournamentModal(true)}
+                      className={"new-tournament-button"}>
+                    Start new tournament?
+                </Button>
+              </div>
+            : <span/>
+        }
         {sortedTournaments.length > 0
             ?
         <div>
-            <Message id={"rules-msg"}
-                     icon={"warning sign"}
-                     header={"Remember the tournament rules!"}
-                     content={"Play to 21, and you must win by 2!"}/>
+            <div className={"tournament-details"}>
+                <Header content={sortedTournaments[0].name}
+                        subheader={"Start date: " + sortedTournaments[0].start_date}/>
+            </div>
             <div className="tournament-container">
                 <div className="tournament-headers">
                     <h3>Quarter-Finals</h3>
@@ -243,25 +300,19 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
                     </ul>  
                 </div>
             </div>
+            <Message id={"rules-msg"}
+                     icon={"warning sign"}
+                     header={"Remember the tournament rules!"}
+                     content={"Play to 21, and you must win by 2!"}/>
         </div>
-            : <Button onClick={() => openNewTournamentModal(true)}
-                      className={"new-tournament-button"}>
-                Start new tournament!
-              </Button>
-        }
-        {sortedTournaments.length > 0 && 
-         sortedTournaments[0].matches[6] &&
-         sortedTournaments[0].matches[6].home_score !== undefined
-            ? <div>
-                <div className={"congrats-div"}>
-                    Congratulations {getWinner()}!
-                </div>
+            : <div>{!props.loading
+                ?
                 <Button onClick={() => openNewTournamentModal(true)}
                       className={"new-tournament-button"}>
-                    Start new tournament?
+                Start new tournament!
                 </Button>
-              </div>
-            : <span/>
+                : <span/>
+            }</div>
         }
         {newTournamentModalOpen
         ? <Modal
@@ -272,20 +323,31 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
                 Start new tournament <Icon name={"trophy"}/>
             </Modal.Header>
             <Modal.Content>
-                <Form onSubmit={() => {
-                        startNewTournament(sortedPlayers.slice(0, 8), newTournamentName);
-                        openNewTournamentModal(false);
-                        props.setForceRefresh(true);
-                    }}>
-                    <Form.Input label={"Tournament name"}
+                <Form onSubmit={() => startNewTournament(sortedPlayers.slice(0, 8), newTournamentName)}>
+                    <Form.Input className={"tournament-name-input"}
+                                label={"Tournament name"}
                                 onChange={(event, data) => setNewTournamentName(data.value)}
                                 required/>
-                    <div>Current ladder positions:</div>
-                    <List ordered>
-                        {getLadderListItems(sortedPlayers)}
-                    </List>
-                    <Form.Button disabled={!newTournamentName}>
-                        Start tournament with these top 8 players
+                    <div id={"new-tournament-ladder-scroller"}>
+                        <Table id={"new-tournament-ladder-table"}>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell>Ladder position</Table.HeaderCell>
+                                    <Table.HeaderCell>Player name</Table.HeaderCell>
+                                    <Table.HeaderCell>Player ELO</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                            <Table.Body>
+                                {getLadderTableRows(sortedPlayers)}
+                            </Table.Body>
+                        </Table>
+                    </div>
+                    <Form.Button disabled={!newTournamentName || startingNewTournament} id={"new-tournament-btn"}>
+                        {`Start tournament with these top 8 players  `}
+                        {startingNewTournament
+                        ? <Icon loading name={"spinner"}/>
+                        : <span/>
+                        }
                     </Form.Button>
                 </Form>
             </Modal.Content>
@@ -318,8 +380,14 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
                                     required/>
                     </Form.Group>
                     <Form.Button className={"confirm-score-button"}
-                                disabled={homePlayerEnteringScore === undefined || awayPlayerEnteringScore === undefined}>
-                        Confirm score
+                                 disabled={homePlayerEnteringScore === undefined ||
+                                           awayPlayerEnteringScore === undefined ||
+                                           confirmingMatchScore}>
+                        {`Confirm score  `}
+                        {confirmingMatchScore
+                        ? <Icon loading name={"spinner"}/>
+                        : <span/>
+                        }
                     </Form.Button>
                 </Form>
             </Modal.Content>
@@ -329,18 +397,19 @@ function Tournament(props: TTDataPropsTypeCombined): JSX.Element {
     );
 }
 
-const getLadderListItems = (players: DbPlayer[]): JSX.Element[] => {
-    const listItems: JSX.Element[] = [];
+const getLadderTableRows = (players: DbPlayer[]): JSX.Element[] => {
+    const tableRows: JSX.Element[] = [];
     for (let i = 0; i < players.length; ++i) {
-        listItems.push(
-        <List.Item className={i <= 7 ? "qualified-player" : "eliminated-player"}>
-            <Icon name={players[i].icon}/> {players[i].name}
-        </List.Item>);
-        if (i === 7) {
-            listItems.push(<div>--------------------------------------------</div>);
-        }
+        tableRows.push(
+            <Table.Row positive={i <= 7} negative={i >= 8}>
+                <Table.Cell>{i + 1}</Table.Cell>
+                <Table.Cell><Icon name={players[i].icon}/> {players[i].name}</Table.Cell>
+                <Table.Cell>{players[i].elo}</Table.Cell>
+            </Table.Row>
+        );
+
     }
-    return listItems;
+    return tableRows;
 };
 
 export default Tournament;
