447753621fe07f481d1ba60268e744424c543298
ChrisNeedham24 <christopherneedham24@gmail.com>
2021-10-23 16:37:22 +1100

    QH-129 Modified reference lines to only display if more than 1 game has been played, and fixed style.

--- src/components/PlayerStatistics/ELOGraph.tsx
+++ src/components/PlayerStatistics/ELOGraph.tsx
@@ -40,20 +40,24 @@ export default function ELOGraph(props: GraphParams): JSX.Element {
                         />
                         <YAxis yAxisId={"1"} domain={[minELO - 50, maxELO + 50]} />
                         <Line type={"monotone"} dataKey={"ELO"} stroke={"#8884d8"} yAxisId={"1"} />
-                        <ReferenceLine
-                            yAxisId={"1"}
-                            y={maxELO}
-                            label={{value: "All time high", fill: "white"}}
-                            stroke={"green"}
-                            strokeDasharray={"3 3"}
-                        />
-                        <ReferenceLine
-                            yAxisId={"1"}
-                            y={minELO}
-                            label={{value: "All time low", fill: "white"}}
-                            stroke={"red"}
-                            strokeDasharray={"3 3"}
-                        />
+                        {graphStats.length > 1 && (
+                            <ReferenceLine
+                                yAxisId={"1"}
+                                y={maxELO}
+                                label={{ value: "All time high", fill: "white" }}
+                                stroke={"green"}
+                                strokeDasharray={"3 3"}
+                            />
+                        )}
+                        {graphStats.length > 1 && (
+                            <ReferenceLine
+                                yAxisId={"1"}
+                                y={minELO}
+                                label={{ value: "All time low", fill: "white" }}
+                                stroke={"red"}
+                                strokeDasharray={"3 3"}
+                            />
+                        )}
                         <Tooltip content={<CustomTooltip />} />
                     </LineChart>
                 </ResponsiveContainer>
