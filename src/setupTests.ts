7e75616b699c915e6c400c5f5a5c83ac09bbff1e
james <s3541398@student.rmit.edu.au>
2022-01-17 00:45:48 +1100

    QH-131 3D test fixed

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,19 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import * as nodeCrypto from "crypto";
+import * as THREE from "three/build/three.module";
+
+// Mock for GL renderer in Jest.
+THREE.WebGLRenderer = class WebGLRenderer {
+    constructor() {
+        // Nothing.
+    }
+
+    setSize = jest.fn();
+    setAnimationLoop = jest.fn();
+    shadowMap = { enabled: true, type: "" };
+    domElement = document.createElement("SPAN");
+};
 
 window.crypto = {
     getRandomValues: function (buffer): NodeJS.ArrayBufferView {
