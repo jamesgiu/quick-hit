73ff6bbba590b814bc325dc590487619a07c770e
ChrisNeedham24 <christopherneedham24@gmail.com>
2022-02-19 16:13:48 +1100

    QH-145 Fixed test.

--- src/components/HallOfFallen/HallOfFallen.test.tsx
+++ src/components/HallOfFallen/HallOfFallen.test.tsx
@@ -2,5 +2,5 @@ import { render } from "@testing-library/react";
 import HallOfFallen from "./HallOfFallen";
 
 it("renders without crashing", () => {
-    render(<HallOfFallen />);
+    render(<HallOfFallen players={[]} />);
 });
