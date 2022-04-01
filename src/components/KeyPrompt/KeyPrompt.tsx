bc151fb762d43d382c4468272a22bd22466cbdf7
jamesgiu <james.giuffrida@gmail.com>
2022-04-01 21:50:46 +1100

    Add missing bracket
    Shouldn't code on my phone

--- src/components/KeyPrompt/KeyPrompt.tsx
+++ src/components/KeyPrompt/KeyPrompt.tsx
@@ -121,7 +121,7 @@ function KeyPrompt(props: KeyPromptProps): JSX.Element {
     };
 
     // Todo: use quickhitpage later
-    if (props.match && !props.match.params.instance === "player")) {
+    if (props.match && !(props.match.params.instance === "player")) {
         const instanceId = props.match.params.instance;
         const authKey = props.match.params.authKey;
 
